ba479fa303445b9253fb31304382d3e9f9635c5d
Bill Jones <williamjones120862@gmail.com>
2025-09-19 16:14:26 -0400

    Added some Bootstrap 5 styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/main'
   devise_for :users
-  resources :articles, except: [:show]
+  resources :articles
   resources :publications, only: [:index, :edit, :destroy]
   resources :exhibitions, only: [:index, :edit, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "exhibitions#index"
+  root "home#main"
 end
